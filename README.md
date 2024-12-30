e9abe8012691e27fb871f9b92f0d8142440a7085
Dmitri Wamback <56200546+DmitriWamback@users.noreply.github.com>
2024-12-30 09:13:54 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gilbert–Johnson–Keerthi
+<p>Using the Gilbert–Johnson–Keerthi Algorithm to detect collisions</p>
+<p>For debugging purposes, the first step is to detect collisions between objects and the camera</p>
---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  GJK
+//
+//  Created by Dmitri Wamback on 2024-12-30.
+//
+
+#include <iostream>
+#include <GL/glew.h>
+#include <glfw3.h>
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
